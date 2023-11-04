e696287f6fa93ad7303d8c62848a0052f747cff7
Carlos Eduardo de Lima <caduu@live.com>
2023-11-04 11:15:13 -0300

    chore: initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,5 @@
-import reactLogo from '../public/react.svg';
-import viteLogo from '../public/vite.svg';
+import { Router } from './Router';
 
-import './App.css';
-
-function App() {
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Template Vite + React</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+export default function App() {
+  return <Router />;
 }
-
-export default App;
---
+++ src/Router/index.tsx
@@ -0,0 +1,7 @@
+export function Router() {
+  return (
+    <div>
+      <h1 className="bg-red-400 text-white">Fincheck Web</h1>
+    </div>
+  );
+}
