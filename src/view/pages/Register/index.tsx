2d499814525f4403bba827348d661cf918d398cb
Carlos Eduardo de Lima <caduu@live.com>
2023-11-07 07:05:58 -0300

    feat: implement `Register` page

--- src/view/pages/Register/index.tsx
+++ src/view/pages/Register/index.tsx
@@ -1,7 +1,39 @@
+import { Link } from 'react-router-dom';
+
+import { Button } from '../../components/Button';
+import { Input } from '../../components/Input';
+
 export function Register() {
   return (
-    <div>
-      <h1>Register</h1>
-    </div>
+    <>
+      <header className="flex flex-col items-center gap-4 text-center">
+        <h1 className="text-2xl text-gray-900 font-bold tracking-[-1px]">
+          Crie sua conta
+        </h1>
+
+        <p className="space-x-2">
+          <span className="text-gray-700 tracking-[-0.5px]">
+            Ja possui uma conta?
+          </span>
+
+          <Link
+            to="/login"
+            className="tracking-[-0.5px] font-medium text-teal-900"
+          >
+            Fazer login
+          </Link>
+        </p>
+      </header>
+
+      <form className="mt-[60px] flex flex-col gap-4">
+        <Input type="text" placeholder="Nome" name="name" />
+        <Input type="email" placeholder="E-mail" name="email" />
+        <Input type="password" placeholder="Senha" name="senha" />
+
+        <Button type="submit" className="mt-2">
+          Criar conta
+        </Button>
+      </form>
+    </>
   );
 }
