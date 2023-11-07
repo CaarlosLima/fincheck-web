debe1055356e6d375071b05dffb16e0e13fd4053
Carlos Eduardo de Lima <caduu@live.com>
2023-11-07 09:42:06 -0300

    feat: implement validation for the `Register` form

--- src/view/pages/Register/index.tsx
+++ src/view/pages/Register/index.tsx
@@ -3,7 +3,11 @@ import { Link } from 'react-router-dom';
 import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
 
+import { useRegisterController } from './useRegisterController';
+
 export function Register() {
+  const { handleSubmit, errors, register } = useRegisterController();
+
   return (
     <>
       <header className="flex flex-col items-center gap-4 text-center">
@@ -25,10 +29,27 @@ export function Register() {
         </p>
       </header>
 
-      <form className="mt-[60px] flex flex-col gap-4">
-        <Input type="text" placeholder="Nome" name="name" />
-        <Input type="email" placeholder="E-mail" name="email" />
-        <Input type="password" placeholder="Senha" name="senha" />
+      <form className="mt-[60px] flex flex-col gap-4" onSubmit={handleSubmit}>
+        <Input
+          type="text"
+          placeholder="Nome"
+          error={errors.name?.message}
+          {...register('name')}
+        />
+
+        <Input
+          type="email"
+          placeholder="E-mail"
+          error={errors.email?.message}
+          {...register('email')}
+        />
+
+        <Input
+          type="password"
+          placeholder="Senha"
+          error={errors.password?.message}
+          {...register('password')}
+        />
 
         <Button type="submit" className="mt-2">
           Criar conta
---
+++ src/view/pages/Register/useRegisterController.ts
@@ -0,0 +1,27 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+const schema = z.object({
+  email: z.string().min(1, 'E-mail é obrigatório').email('E-mail é inválido'),
+  name: z.string().min(1, 'Nome é obrigatório'),
+  password: z.string().min(8, 'Senha deve conter pelo menos 8 dígitos'),
+});
+
+type FormData = z.infer<typeof schema>;
+
+export function useRegisterController() {
+  const {
+    handleSubmit: hookFormHandleSubmit,
+    formState: { errors },
+    register,
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
+
+  const handleSubmit = hookFormHandleSubmit((data) => {
+    console.log(data);
+  });
+
+  return { handleSubmit, errors, register };
+}
