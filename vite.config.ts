5f7fa17927c99b1c92f7a35d9aece4c130f0563d
Carlos Eduardo de Lima <caduu@live.com>
2024-09-28 17:51:12 -0300

    feat: add proxy to api

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,23 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import react from '@vitejs/plugin-react-swc';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  server: {
-    open: true,
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd()) || {};
+
+  return {
+    plugins: [react(), tsconfigPaths()],
+    server: {
+      open: true,
+      proxy: {
+        '/api': {
+          target: env.VITE_BASE_API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+  };
 });
