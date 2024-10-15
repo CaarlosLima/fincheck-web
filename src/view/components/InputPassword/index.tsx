9d29e809bd11a20e283fc5cd72b2f47c5bb519c7
Carlos Eduardo de Lima <caduu@live.com>
2024-10-15 14:31:33 -0300

    fix: fix `InputPassword` button

--- src/view/components/InputPassword/index.tsx
+++ src/view/components/InputPassword/index.tsx
@@ -3,7 +3,7 @@ import { forwardRef, useState } from 'react';
 import { EyeIcon } from 'src/view/components/icons/EyeIcon';
 import { Input, InputProps } from 'src/view/components/Input';
 
-type InputPasswordProps = Omit<InputProps, 'type' | 'ref'>;
+type InputPasswordProps = Omit<InputProps, 'type' | 'ref' | 'suffix'>;
 
 export const InputPassword = forwardRef<HTMLInputElement, InputPasswordProps>(
   (props, ref) => {
@@ -14,19 +14,22 @@ export const InputPassword = forwardRef<HTMLInputElement, InputPasswordProps>(
     };
 
     return (
-      <div className="relative">
-        <Input {...props} ref={ref} type={showPassword ? 'text' : 'password'} />
-
-        <button
-          type="button"
-          className="absolute w-5 top-1/2 -translate-y-1/2 right-3 flex items-center justify-center"
-          title="Mostrar valores"
-          onClick={toggleShowPassword}
-          tabIndex={-1}
-        >
-          <EyeIcon open={showPassword} className="stroke-gray-900" />
-        </button>
-      </div>
+      <Input
+        {...props}
+        ref={ref}
+        type={showPassword ? 'text' : 'password'}
+        suffix={
+          <button
+            type="button"
+            className="absolute w-5 top-1/2 -translate-y-1/2 right-3 flex items-center justify-center"
+            title={`${showPassword ? 'Ocultar' : 'Mostrar'} senha`}
+            onClick={toggleShowPassword}
+            tabIndex={-1}
+          >
+            <EyeIcon open={!showPassword} className="stroke-gray-900" />
+          </button>
+        }
+      />
     );
   },
 );
